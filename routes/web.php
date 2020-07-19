900e98a54ab1398587e49d9c0b012f0d4a1a0cb1
Mehmet Dogukan Sevinc <nextandbackacademy@gmail.com>
2020-07-19 16:56:13 +0300

    second

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('ilk',function (){
+    return view('welcome');
+});
+
+Route::get('ilk',function (){
+   return "İlk örnek";
+})->name('deneme');
+
+Route::get('uc',function (){
+    $url = \route('deneme');
+    return $url;
+});
+
+Route::get('yazilar/{id?}/{title?}',function ($id="",$title=""){
+    return 'Yazilar'.$id.' '.$title;
+});
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('yönetim',function (){
+       return "Yönetim";
+    });
+    Route::get('yetkiler',function (){
+       return "Yetkiler";
+    });
+});
+
+Route::middleware(['auth'])->group(function (){
+    Route::get('gizli',function (){
+       return "Gizli";
+    });
+});
